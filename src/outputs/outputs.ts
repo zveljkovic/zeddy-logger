2fec1ebf9575be5a79998a69c18c470dbcdbecbc
Zlatibor Veljkovic <zveljkovic@hotmail.com>
2025-06-07 21:51:53 +0200

    Fix for JsonOutput availability

--- src/outputs/outputs.ts
+++ src/outputs/outputs.ts
@@ -1,4 +1,5 @@
 import {ConsoleOutput} from './console-output';
 import {FileOutput} from './file-output';
+import {JsonOutput} from './json-output';
 
-export {ConsoleOutput, FileOutput};
+export {ConsoleOutput, FileOutput, JsonOutput};
