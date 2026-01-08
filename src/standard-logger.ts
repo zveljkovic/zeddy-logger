276cc1dcbf8ce34b0a2e394316c63929e3625725
Zlatibor Veljkovic <zveljkovic@hotmail.com>
2026-01-08 08:03:28 +0100

    Standard Logger min level update

--- src/standard-logger.ts
+++ src/standard-logger.ts
@@ -1,6 +1,13 @@
 import {LoggerBase} from './logger-base';
 import type {LogOutput} from './logger-types';
 
+const numericLogLevel = {
+  debug: 0,
+  info: 1,
+  warning: 2,
+  error: 3,
+};
+
 export const LogLevel = {
   info: {name: 'log-level', value: 'info'},
   debug: {name: 'log-level', value: 'debug'},
@@ -9,23 +16,32 @@ export const LogLevel = {
 };
 
 export class StandardLogger extends LoggerBase {
-  constructor(logOutputs: LogOutput[]) {
+  private minLogNumeric: number = 1;
+  constructor(logOutputs: LogOutput[], minLogLevel?: keyof typeof LogLevel) {
     super(logOutputs);
+    if (minLogLevel) {
+      this.minLogNumeric = numericLogLevel[minLogLevel];
+    }
   }
 
+
   public info(message: string, data?: any) {
+    if (this.minLogNumeric > 1) return;
     void this.baseLog({tags: [LogLevel.info], data, message});
   }
 
   public debug(message: string, data?: any) {
+    if (this.minLogNumeric > 0) return;
     void this.baseLog({tags: [LogLevel.debug], data, message});
   }
 
   public warning(message: string, data?: any) {
+    if (this.minLogNumeric > 2) return;
     void this.baseLog({tags: [LogLevel.warning], data, message});
   }
 
   public error(message: string, data?: any) {
+    if (this.minLogNumeric > 3) return;
     void this.baseLog({tags: [LogLevel.error], data, message});
   }
 }
